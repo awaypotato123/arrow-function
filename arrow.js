60393cb80f91f97a792d37d7afb5b5d21b230c64
Riyadh <riyadhahmed324@gmail.com>
2023-09-14 13:42:17 -0400

    second commit

--- arrow.js
+++ arrow.js
@@ -1,8 +1,19 @@
-function sum1(a, b) {
+// function sum1(a, b) {
+//     return a + b;
+//     }
+//     let result = sum1(1, 2);
+//     console.log(result);
+
+const sum1 = (a, b) => {
     return a + b;
     }
     let result = sum1(1, 2);
-    console.log(result);
+    console.log(result)
 
 
     // The first modification by Koji Abe 2023-09-13
+
+    //The Second modification by Riyadh Ahmed 2023-09-14
+
+    //normal function was commented out 
+    //arrow function was added
